3a9f7c15d4a1474561badbb38121a9910dbc5a27
Marc <marc.ferbos@gmail.com>
2024-03-11 14:19:54 +0100

    Installation des routes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from './Pages/Login';
+import Profile from './Pages/Profile';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
