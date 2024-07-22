65fe32d47d025f8299b81398b5d2d532568bdbd1
Marc <marc.ferbos@gmail.com>
2024-07-22 17:11:39 +0200

    Optimisation images green code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -21,9 +21,9 @@ function App() {
                 </div>
                 <section className="features">
                     <h2 className="sr-only">Features</h2>
-                    <FeatureItem icon="./imgnew/icon-chat.png" iconalt="Chat Icon" title="You are our #1 priority" description="Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes." />
-                    <FeatureItem icon="./imgnew/icon-money.png" iconalt="Chat Icon" title="More savings means higher rates" description="The more you save with us, the higher your interest rate will be!" />
-                    <FeatureItem icon="./imgnew/icon-security.png" iconalt="Chat Icon" title="Security you can trust" description="We use top of the line encryption to make sure your data and money is always safe." />
+                    <FeatureItem icon="./imgnew/icon-chat.webp" iconalt="Chat Icon" title="You are our #1 priority" description="Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes." />
+                    <FeatureItem icon="./imgnew/icon-money.webp" iconalt="Chat Icon" title="More savings means higher rates" description="The more you save with us, the higher your interest rate will be!" />
+                    <FeatureItem icon="./imgnew/icon-security.webp" iconalt="Chat Icon" title="Security you can trust" description="We use top of the line encryption to make sure your data and money is always safe." />
                 </section>
             </main>
             <Footer />
